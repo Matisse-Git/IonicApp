23befdc65c86b264cd7e83007d9567c23d47dd31
Matisse Faes <matttske@gmail.com>
2019-12-26 00:34:46 +0100

    can fetch user info

--- IonicApp/src/app/tab1/tab1.page.ts
+++ IonicApp/src/app/tab1/tab1.page.ts
@@ -5,7 +5,6 @@ import { Router } from '@angular/router'
 import { IGame } from '../app.module';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
 
-
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
@@ -20,18 +19,7 @@ export class Tab1Page {
   currentGames: IGame[] = [];
 
   ngOnInit(){
-    this.service.getGames().subscribe(game =>{
-      game.results.forEach(element => {
-        this.currentGames.push(element);
-      });
-    })
+
   }
 
-  getGames(){
-    this.service.getGames().subscribe(game =>{
-      game.results.forEach(element => {
-        this.currentGames.push(element);
-      });
-    })
-  }
 }
--- IonicApp/src/app/tab3/tab3.page.ts
+++ IonicApp/src/app/tab3/tab3.page.ts
@@ -1,7 +1,8 @@
 import { Component } from '@angular/core';
 import { APIService } from '../api.service'
+var Rawger = require('rawger');
 
-
+declare var require: any
 @Component({
   selector: 'app-tab3',
   templateUrl: 'tab3.page.html',
@@ -11,8 +12,24 @@ export class Tab3Page {
 
   constructor(private service: APIService) {}
 
-  ngOnInit(){
+  async ngOnInit(){
+    var rawger = await Rawger();
+    var { users } = rawger;
+    var playing = (await users('Matttske').games('playing')).get();
+    var toplay = (await users('Matttske').games('toplay')).get();
+    var beaten  = (await users('Matttske').games('beaten ')).get();
+    var dropped = (await users('Matttske').games('dropped')).get();
+    var yet = (await users('Matttske').games('yet ')).get();
+    var owned  = (await users('Matttske').games('owned ')).get();
+    var collections = (await users('Matttske').collections()).get();
     
+    console.log(playing);
+    console.log(toplay);
+    console.log(beaten);
+    console.log(dropped);
+    console.log(yet);
+    console.log(owned);
+    console.log(collections)
   }
 
 
